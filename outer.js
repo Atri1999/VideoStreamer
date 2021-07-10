e4e10298dc0daea38405e3ae19322c561ec8170a
Atri Chakraborty <chakrabortyatri1999@gmail.com>
2021-07-10 11:47:25 +0530

    The Streaming feature is added. It can now stream the selected video

---
+++ outer.js
@@ -0,0 +1,31 @@
+const path=require("path")
+const fs=require('fs')
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+const { dir } = require("console");
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const direc=path.join(__dirname,'videos')
+
+fs.readdir(direc,(err,files)=>{
+
+    files.forEach((file)=>{
+        console.log(file)
+        ffmpeg({ source:__dirname+'/videos/'+file })
+            .on('error',(err)=>{
+                console.log("error occured"+err);
+            })
+            .on('end',()=>{
+                console.log("completed")
+            })
+            .on('filenames',(filename)=>{
+                console.log(filename)
+            })
+            .takeScreenshots({
+                filename:`${file}.jpg`,
+                timemarks:[10]
+            },'./public/thumbnails');
+    })
+    
+})
